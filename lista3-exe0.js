71a8d45cae7e5c7a3de0f242c1cc63d794af7e09
Guilherme Maranha <guilhermemaranha@hotmail.com.br>
2020-09-15 20:37:42 -0300

    EX 0 da lista 3 ATUALIZADO

--- lista3-exe0.js
+++ lista3-exe0.js
@@ -2,30 +2,59 @@ chefe()
 function chefe(){
     // declarar a matriz 
     let matriz = [] // uma matriz é um vetor
-    //matriz de 3 linhas
-    matriz[0] = [] // cada linha da matriz é uma matriz
-    matriz[1] = []
-    matriz[2] = []
-    cadastra(matriz) //cadastra 3 alunos
-    consultaMediaAlunos(matriz)
+    // vamos criar o vetor alunos
+    let vetor = []
+    cadastra(matriz,vetor) //cadastra 3 alunos
+    consultaMediaAlunos(matriz,vetor)
+    consultaMediaProvas(matriz)
+    consultaMaiorNota(matriz, vetor)
 }
-function cadastra(mat){
+function cadastra(mat,vet){
     for(let i = 0;i<3;i++){
-        alert(`Informe 4 notas do aluno ${i+1}`)
-        for(let j = 0;j<4;j++){
+        let objeto = new Object()
+        objeto.nome = prompt(`Informe o nome do aluno`)
+        objeto.rg = prompt(`Informe o RG do aluno`)
+        objeto.media = 0
+        vet.push(objeto) //insere o objeto no vet
+        mat[i] = []
+        alert(`Informe as 4 notas do aluno ${objeto.nome}`)
+        for(let j = 0;j<4;j++){ 
             mat[i][j] = Number(prompt(`Infome a ${j+1}ª nota`))
         }
     }
 }
-function consultaMediaAlunos(mat){
-    let medias = [0,0,0]
+function consultaMediaAlunos(mat,vet){
     for(let i = 0;i<3;i++){
         for(let j = 0;j<4;j++){
             //soma no vetor na posicao i
-            medias[i] = medias[i] + mat[i][j]
+            vet[i].media = vet[i].media + mat[i][j]
         }
         //acabaram as notas do aluno i
-        medias[i] = medias[i] / 4
-        console.log(`Media do aluno ${i+1} = ${medias[i]}`)
+        vet[i].media = vet[i].media / 4
+        console.log(`Media do aluno ${i+1} = ${vet[i].media}`)
+    }
+}
+
+function consultaMediaProvas(mat){
+    for(let j = 0;j<4;j++){
+        let media = 0
+        for(let i = 0;i<3;i++){
+            //soma cada prova
+            media = media + mat[i][j]
+        }
+        console.log(`Media da prova ${j+1} = ${media/3}`)
+    }
+}
+function consultaMaiorNota(mat, vet){
+    for(let j = 0;j<4;j++){ 
+        let MaiorNota = mat[0][j]   //assumo que a maior nota e do primeiro alnuo
+        let NomeMaiorNota = vet[0].nome   //assumo que o primeiro aluno tem a maior nota
+        for(let i = 0;i<3;i++){
+            if(mat[i][j] > MaiorNota){  //atualiza
+                MaiorNota = mat[i][j]
+                NomeMaiorNota = vet[i].nome
+            }
+        }
+        console.log(`Na prova ${j+1} o aluno ${NomeMaiorNota} tem a maior nota = ${MaiorNota}`)
     }
 }
